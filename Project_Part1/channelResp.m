607678be18f72793c7d1b6190641f13cccad4cd9
yuxuanx <xiayuxuan1993@outlook.com>
2016-02-04 10:03:42 +0100

    minor changes

--- Project_Part1/channelResp.m
+++ Project_Part1/channelResp.m
@@ -1,11 +1,14 @@
-function [] = channelResp( x, fdts, L, M, N )
+function [] = channelResp( fdts, L, M, N )
 %calculate channel response for discrete-time channel model
+Ns = 10000;
 C = zeros(N,M);
 Cl = zeros(N,M);
 for l = 1:L
     tao = (l-1)*2;
+    x = (randn(Ns, 1) + sqrt(-1)*randn(Ns, 1))/sqrt(2); % Gaussian noise
     for k = 1:N
         expo = exp(-1j*2*pi*k*tao/N);
+        
         cl = rayleighFading( x, M/2, fdts )';
         Cl(k,:) = cl*expo;
     end
--- Project_Part1/tfVarying.m
+++ Project_Part1/tfVarying.m
@@ -2,18 +2,17 @@
 M = 300; % time samples
 N = 64; % frequency samples
 L = 3; % number of taps
-Ns = 10000;
-x = (randn(Ns, 1) + sqrt(-1)*randn(Ns, 1))/sqrt(2); % Gaussian noise
+
 %% 9 combinations for L = 1,2,3 fdts = 0.1,0.01,0.005
-channelResp( x, 0.1, 1, M, N );
-channelResp( x, 0.01, 1, M, N );
-channelResp( x, 0.005, 1, M, N );
-channelResp( x, 0.1, 2, M, N );
-channelResp( x, 0.01, 2, M, N );
-channelResp( x, 0.005, 2, M, N );
-channelResp( x, 0.1, 4, M, N );
-channelResp( x, 0.01, 7, M, N );
-channelResp( x, 0.001 , 3, M, N );
+channelResp( 0.1, 1, M, N );
+channelResp( 0.01, 1, M, N );
+channelResp( 0.005, 1, M, N );
+channelResp( 0.1, 2, M, N );
+channelResp( 0.01, 2, M, N );
+channelResp( 0.005, 2, M, N );
+channelResp( 0.1, 3, M, N );
+channelResp( 0.01, 3, M, N );
+channelResp( 0.005 , 3, M, N );
 
 
 
